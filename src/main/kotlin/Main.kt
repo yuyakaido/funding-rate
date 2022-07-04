9cd61fa4817a05aeda86da34dbf2417f6ba28dcf
Yuya Kaido <yuyakaido@gmail.com>
2022-07-05 01:27:41 +0900

    Calculate funding rate ranking

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,46 @@
+import java.io.File
+import java.math.BigDecimal
+
 fun main() {
-    println("Hello!")
+    var results = emptyList<Result>()
+
+    val directory = File("${System.getProperty("user.dir")}/src/main/resources")
+    directory.listFiles()?.forEach { file ->
+        val lines = file.readLines()
+        val headers = lines.first().replace("\"", "").split(",")
+        val fundingRateIndex = headers.indexOf("Funding Rate")
+        val records = lines.subList(1, 200)
+
+        var sumOfFundingRate = BigDecimal.ZERO
+        var minOfFundingRate = BigDecimal.ZERO
+        var maxOfFundingRate = BigDecimal.ZERO
+        records.forEach { record ->
+            val values = record.replace("\"", "").replace("%", "").split(",")
+            val fundingRateValue = BigDecimal(values[fundingRateIndex])
+            sumOfFundingRate += fundingRateValue
+            if (fundingRateValue < minOfFundingRate) {
+                minOfFundingRate = fundingRateValue
+            }
+            if (maxOfFundingRate < fundingRateValue) {
+                maxOfFundingRate = fundingRateValue
+            }
+        }
+        results = results.plus(
+            Result(
+                file = file,
+                sum = sumOfFundingRate,
+                min = minOfFundingRate,
+                max = maxOfFundingRate
+            )
+        )
+    }
+
+    results.filter { it.sum >= BigDecimal.ZERO }.sortedBy { it.sum }.forEach { println(it) }
 }
+
+data class Result(
+    val file: File,
+    val sum: BigDecimal,
+    val min: BigDecimal,
+    val max: BigDecimal
+)
