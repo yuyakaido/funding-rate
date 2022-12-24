ba4e2a8efbf4fce78e0a3422a4f64951d072a659
Yuya Kaido <yuyakaido@gmail.com>
2022-12-24 17:25:34 +0900

    Update history files

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,8 +9,11 @@ fun main() {
         val lines = file.readLines()
         val headers = lines.first().replace("\"", "").split(",")
         val fundingRateIndex = headers.indexOf("Funding Rate")
-        val records = lines.subList(1, 100)
+        val records = lines.subList(1, 183)
 
+        var plusCount = 0
+        var minusCount = 0
+        var bigMinusCount = 0
         var sumOfFundingRate = BigDecimal.ZERO
         var minOfFundingRate = BigDecimal.ZERO
         var maxOfFundingRate = BigDecimal.ZERO
@@ -18,6 +21,14 @@ fun main() {
             val values = record.replace("\"", "").replace("%", "").split(",")
             val fundingRateValue = BigDecimal(values[fundingRateIndex])
             sumOfFundingRate += fundingRateValue
+            if (fundingRateValue < BigDecimal.ZERO) {
+                minusCount++
+            } else {
+                plusCount++
+            }
+            if (fundingRateValue < BigDecimal(-0.1)) {
+                bigMinusCount++
+            }
             if (fundingRateValue < minOfFundingRate) {
                 minOfFundingRate = fundingRateValue
             }
@@ -27,7 +38,12 @@ fun main() {
         }
         results = results.plus(
             Result(
-                file = file,
+                name = file.name,
+                allCount = records.size,
+                plusCount = plusCount,
+                minusCount = minusCount,
+                bigMinusCount = bigMinusCount,
+                winingRate = (plusCount.toDouble() / records.size.toDouble() * 100).toInt(),
                 sum = sumOfFundingRate,
                 min = minOfFundingRate,
                 max = maxOfFundingRate
@@ -39,7 +55,12 @@ fun main() {
 }
 
 data class Result(
-    val file: File,
+    val name: String,
+    val allCount: Int,
+    val plusCount: Int,
+    val minusCount: Int,
+    val bigMinusCount: Int,
+    val winingRate: Int,
     val sum: BigDecimal,
     val min: BigDecimal,
     val max: BigDecimal
